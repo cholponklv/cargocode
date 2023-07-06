0806cb5cfb2665fbed2cc39238ce2443d0437b0c
cholponklv <cbaiel@mail.ru>
2023-07-06 20:10:51 +0600

    заказы 3.0

--- order/serializers.py
+++ order/serializers.py
@@ -1,12 +1,29 @@
 from rest_framework import serializers
+from driver.serializers import DriverSerializer
+from .models import Order, OrderOffer, CargoType
+from company.serializers import CompanySerializer
+from shipper.serializers import ShipperSerializer
 
-from .models import Order,OrderOffer
+class CargoTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CargoType
+        fields = '__all__'
 
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
+    
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["cargo_type"]=CargoTypeSerializer(instance.cargo_type).data
+        data["driver"]=DriverSerializer(instance.driver).data 
+        data["company"]=CompanySerializer(instance.company).data 
+        data["shipper"]=ShipperSerializer(instance.shipper).data 
+        return data
+
+
 
 class OrderOfferSerializer(serializers.ModelSerializer):
     class Meta:
