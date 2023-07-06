3bafcbbe71771d00aac66a0f1bb2d4efe55239fb
Anas <anas.ergeshov.98@gmail.com>
2023-07-07 01:47:47 +0600

    cargo-types

--- order/urls.py
+++ order/urls.py
@@ -7,6 +7,7 @@ router = DefaultRouter()
 
 router.register('orders', viewset=views.OrdersViewSet, basename='orders')
 router.register('orderoffers', viewset=views.OrdersOfferViewSet, basename='orders')
+router.register('cargo-types', viewset=views.CargoTypeViewSet, basename='cargo_types')
 urlpatterns = [
     path('v1/', include(router.urls)),
     # path('v1/orders/<int:pk>/offers/<int:offer_id>/accept/', views.OrdersViewSet.as_view({'post': 'accept'}), name='order-accept'),
